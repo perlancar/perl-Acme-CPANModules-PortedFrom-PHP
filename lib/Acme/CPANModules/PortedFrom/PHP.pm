b123c8026da6c2645352b89a9c54d7d94aa1a14f
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-14 18:40:27 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/PortedFrom/PHP.pm
@@ -0,0 +1,21 @@
+package Acme::CPANModules::PortedFrom::PHP;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Modules/applications that are ported from (or inspired by) ".
+        "PHP libraries",
+    description => <<'_',
+
+If you know of others, please drop me a message.
+
+_
+    entries => [
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
