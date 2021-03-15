d576eecfe51013b19392c47eb2bb8d977b6db936
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-03-15 13:00:21 +0700

    [doc] Mention some other Acme::CPANModules::PortedFrom::* modules

--- lib/Acme/CPANModules/PortedFrom/PHP.pm
+++ lib/Acme/CPANModules/PortedFrom/PHP.pm
@@ -20,3 +20,13 @@ _
 # ABSTRACT:
 
 =head1 DESCRIPTION
+
+=head1 prepend:SEE ALSO
+
+More on the same theme of modules ported from other languages:
+L<Acme::CPANModules::PortedFrom::Clojure>,
+L<Acme::CPANModules::PortedFrom::Go>,
+L<Acme::CPANModules::PortedFrom::Java>,
+L<Acme::CPANModules::PortedFrom::NPM>,
+L<Acme::CPANModules::PortedFrom::Python>,
+L<Acme::CPANModules::PortedFrom::Ruby>.
